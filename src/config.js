400e64b2567a605f6fafe4006065dbe05c005067
lexysticks <olemmanuel@1528>
2026-02-06 18:01:58 +0100

    push to vercel

--- src/config.js
+++ src/config.js
@@ -1,10 +1,12 @@
-// src/config.js
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+const LOCAL_URL = "http://127.0.0.1:8000";
+const PROD_URL = "https://housing-backend-new.onrender.com";
+
+const BASE_URL = import.meta.env.DEV ? LOCAL_URL : PROD_URL;
 
 export const endpoints = {
   properties: `${BASE_URL}/properties/`,
-  lands: `${BASE_URL}/Lands/`,
+  lands: `${BASE_URL}/lands/`,
   shortlet: `${BASE_URL}/shortlet/`,
   buy: `${BASE_URL}/buy/`,
   rent: `${BASE_URL}/rent/`,
